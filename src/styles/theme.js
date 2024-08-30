7e39c8bd419c0802c297899a2674c7dd1e70592b
tiago-silver <tiagopereirasilva5425@gmail.com>
2024-08-29 22:15:48 -0300

    rocknotes front

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+export default {
+    COLORS: {
+        BACKGROUND_900: "#0D0C0F",
+        BACKGROUND_800: "#1C1B1E",
+        BACKGROUND_700: "#262529",
+
+        RED: "#FF859B",
+        GRAY: "#948F99",
+        WHITE: "#F4EDE8"
+
+    }
+}
